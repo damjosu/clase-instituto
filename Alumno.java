ac13b3c21e09282bc5e53b7103ed82d80f4909eb
damjosu <damjosu@gmail.com>
2015-03-09 09:55:14 +0100

    primer commit

---
+++ Alumno.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Alumno here.
+ * 
+ * @author (josu) 
+ * @version (09/03/2015)
+ */
+public class Alumno
+{
+
+    /**
+     * Constructor for objects of class Alumno
+     */
+    public Alumno()
+    {
+        
+    }
+
+}
