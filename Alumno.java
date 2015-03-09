b6e03107b70e290ded184d5555a3764df4d208d3
damjosu <damjosu@gmail.com>
2015-03-09 10:20:15 +0100

    añadido metodo para introducir notas

--- Alumno.java
+++ Alumno.java
@@ -7,12 +7,30 @@
  */
 public class Alumno
 {
-
+    private String nombre;
+    private int edad;
+    private int numeroClase;
+    private int notas[];
     /**
      * Constructor for objects of class Alumno
      */
-    public Alumno()
-    {
+    public Alumno(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;    
+        notas = new int[0];
+    }
+
+    public void addNota(int nota) {
+        int temp[] = new int[notas.length + 1];
+        int i;
+        for(i = 0; i < notas.length; i++) {
+            temp[i] = notas[i];
+        }
+        temp[i] = nota;
+        notas = temp;
+    }
+         
+    public int media() {
         
     }
 
