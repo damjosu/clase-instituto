f2ea6a245cc7af48443893a80d69b8a6c0fdae89
BuildTools <unconfigured@null.spigotmc.org>
2015-03-10 20:56:30 +0100

    añadido metodo para ver la media, ver si el alumno esta aprobado o no, otro para mostrar los datos del alumno, y creada la clase test.

--- Alumno.java
+++ Alumno.java
@@ -1,25 +1,39 @@
 
 /**
- * Write a description of class Alumno here.
- * 
+ * Clase Alumno en la que se puede ver el nombre, edad, numero de clase, añadir notas
+ * ver la nota media y si esta aprobado o no.
  * @author (josu) 
  * @version (09/03/2015)
  */
 public class Alumno
 {
+    // Nombre del alumno.
     private String nombre;
+    // Edad del alumno.
     private int edad;
+    // Numero de clase del alumno.
     private int numeroClase;
+    // Todas las notas del alumno.
     private int notas[];
+    // Nota minima para aprobar.
+    public static final int NOTA_APROBADO = 5;
+    // El número de alumnos en la clase.
+    public static int numeroAlumnos = 0;    
     /**
-     * Constructor for objects of class Alumno
+     * Constructor de la clase Alumno.
      */
-    public Alumno(String nombre, int edad){
+    public Alumno(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;    
         notas = new int[0];
+        numeroAlumnos += 1;
+        numeroClase = numeroAlumnos;
     }
 
+    /**
+     * Añade una nota al alumno.
+     * @param nota La nota a añadir.
+     */
     public void addNota(int nota) {
         int temp[] = new int[notas.length + 1];
         int i;
@@ -29,9 +43,48 @@ public class Alumno
         temp[i] = nota;
         notas = temp;
     }
-         
-    public int media() {
-        
-    }
 
+    /**
+     * Calcula la media de notas del alumno.
+     * @return la nota media.
+     */
+    public int media() {
+        int media = 0;
+        int i;
+        if (notas.length != 0) {
+            for (i = 0; i < notas.length; i++) {
+                media += notas[i];
+            }
+            media = (media / i);
+        }   
+        return media;
+    }
+    
+    /**
+     * Devuelve true si esta aprobado, false en caso contrario.
+     * @return si está aprobado o no.
+     */
+    public boolean estado() {
+        return (media() >= NOTA_APROBADO);
+    }
+    
+    /**
+     * Muestra todos los datos de los alumnos.
+     */
+    public void mostrarDatos() {
+        String todasLasNotas = "[]";
+        int media = 0;
+        if (notas.length != 0) {
+            todasLasNotas = "[" + notas[0];
+            for (int i = 1; i < notas.length; i++) {            
+                todasLasNotas += ", " + notas[i];
+            }
+            todasLasNotas += "]"; 
+        }   
+        
+        if (media() != 0) {
+            media = media();
+        }
+        System.out.println("Alumno: " + nombre + "\n Edad: " + edad + "\n Número de clase: " + numeroClase + "\n Notas: " + todasLasNotas + "\n Nota media: " + media + "\n ");
+    }
 }
--- Test.java
+++ Test.java
@@ -1,33 +1,46 @@
-
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
 /**
- * Write a description of class Test here.
- * 
+ * Clase Test
  * @author (Josu) 
  * @version (09/03/2015)
  */
 public class Test
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    // Edad del alumno.
+    private int edad;
+    // Nombres de los alumnos de la clase.
+    private ArrayList<String> nombres;
+    // Alumno de la clase.
+    private Alumno alumno;
+    private Random rnd;   
+    private static int CANTIDAD_NOTAS;
+    public static final int CANTIDAD_ALUMNOS = 5;
+    
     /**
-     * Constructor for objects of class Test
+     * Constructor de la clase Test
      */
     public Test()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+        rnd = new Random();
+        nombres = new ArrayList<>();
+        nombres.add("Pepe");
+        nombres.add("María");
+        nombres.add("Juan");
+        nombres.add("Sara");
+        nombres.add("Alberto");
+        Collections.shuffle(nombres);    
+        int nota;
+        for (int i = 0; i < CANTIDAD_ALUMNOS; i++) {
+            edad = (rnd.nextInt(19) + 50);
+            CANTIDAD_NOTAS = rnd.nextInt(7);
+            alumno = new Alumno(nombres.get(i), edad);
+            for (int cont = 0; cont < CANTIDAD_NOTAS; cont++) {
+                nota = rnd.nextInt(11);
+                alumno.addNota(nota);
+            }
+            alumno.mostrarDatos();
+        }
+    }  
 }
